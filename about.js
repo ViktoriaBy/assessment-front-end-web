7cdb0027df6685fd9f1d8e0a3b138b59ed226f02
ViktoriaB <v.chamborant@hotmail.com>
2022-01-21 11:03:05 -0600

    about.html done

--- about.js
+++ about.js
@@ -4,11 +4,23 @@ console.log("hello world");
 
 function handleSubmit(evt) {
 	evt.preventDefault();
-	
-	console.log('form submit');
+	/**ALERT WINDOW FOR SUBMIT BTN */
+	alert('Form has been submited successfully');
 }
-
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+
+
+
+//MOUSEOVER DUCK ALERT
+function mouseOver(){
+
+	alert("You have a wonderfull smile!");
+}
+
+document.getElementById("duck").addEventListener("mouseover", mouseOver);
+
