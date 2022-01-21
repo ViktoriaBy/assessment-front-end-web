f75094e67d6be0d98b304aeb7b4891ef7744ca0d
ViktoriaB <v.chamborant@hotmail.com>
2022-01-21 11:12:56 -0600

    Added functionality to the BTN LIST

--- profile.js
+++ profile.js
@@ -1,1 +1,22 @@
 /**CHECK FOR CLICK ON BTN-LIST BUTTONS */
+
+/** BTN COLOR */
+let btn1 = document.querySelector('#color').addEventListener('click', btn1Alert)
+
+function btn1Alert(){
+    alert('My favorite color is sage green.');
+}
+
+/**BTN PLACE */
+let btn2 = document.querySelector('#place').addEventListener('click', btn2Alert)
+
+function btn2Alert(){
+    alert('My favorite place is with a great nature and less people.')
+}
+
+/**BTN RITUAL */
+let btn3 = document.querySelector('#ritual').addEventListener('click', btn3Alert)
+
+function btn3Alert(){
+    alert('My favorite ritual is going out to eat every Saturday with my husband.')
+}
